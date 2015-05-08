95f6e7fdce5de9ad3eb801069fb30fc455aa8147
utamaro <world.has.been.changed@gmail.com>
2015-05-08 20:04:41 +0900

    avoided overwrap memory copy.

--- src/e3x/cs1a/aes.c
+++ src/e3x/cs1a/aes.c
@@ -3,7 +3,8 @@
 
 void aes_128_ctr(unsigned char *key, size_t length, unsigned char iv[16], const unsigned char *input, unsigned char *output)
 {
-  memcpy(output,input,length);
+  if(input!=output)
+      memcpy(output,input,length);
   aes_128_ctr_encrypt(key,iv,output,length);
 }
 
