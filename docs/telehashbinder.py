e2659fddc67fe87582bf3d99d6c8dc67f4d5803f
utamaro <world.has.been.changed@gmail.com>
2015-05-19 14:52:31 +0900

    for pep

--- docs/telehashbinder.py
+++ docs/telehashbinder.py
@@ -1,31 +1,35 @@
 """
   telehash binder implmeneted in C++.
 """
-def init(port,channel__handler_factory,broadcast_handler):
+
+
+def init(port, channel__handler_factory, broadcast_handler):
     """
     init.
 
-    :param int port: port number to be listened packets. if 0, port number is 
+    :param int port: port number to be listened packets. if 0, port number is.
     :param Method channel_handler_factory: method for creating ChannelHandler.
-                                           called when requested openning 
+                                           called when requested openning.
                                            channel.
     :param method broadcast_handler: method for handling broadcasted message.
     """
     pass
 
-def open_channel(cobj, location,name,handler):
+
+def open_channel(cobj, location, name, handler):
     """
     open a channel associated a channel name.
-    
+
     :param Object cobj: pointer of StorjTelehash instnace returned by init()
     :param str location: json str where you want to open a channel.
     :param str name: channel name that you want to open .
-    :param Method handler: method for handling channel packets. 
+    :param Method handler: method for handling channel packets.
                            called every time when receiving packets.
     """
     pass
 
-def add_broadcaster(cobj,location,add):
+
+def add_broadcaster(cobj, location, add):
     """
     send a broadcast request to broadcaster.
     After calling this method, broadcast messages will be send continually.
@@ -33,11 +37,12 @@ def add_broadcaster(cobj,location,add):
     :param Object cobj: pointer of StorjTelehash instnace returned by init()
     :param str location: json str where you want to request a broadcast.
     :param  int add: if 0, request to not to  broadcast. request to broaadcast
-                     if others. 
+                     if others.
     """
     pass
 
-def broadcast(cobj,location,message):
+
+def broadcast(cobj, location, message):
     """
     broadcast a message.
 
@@ -47,6 +52,7 @@ def broadcast(cobj,location,message):
     """
     pass
 
+
 def start(cobj):
     """
     star to receive netowrk packets in the current thread..
@@ -55,14 +61,16 @@ def start(cobj):
     """
     pass
 
+
 def set_stopflag(cobj, stop):
     """
     set stopFlag that stop/continue to receive network packets loop..
-    
+
     :param Object cobj: pointer of StorjTelehash instnace returned by init()
     :param int flag: 1 if to stop. others if to run.
     """
     pass
+
 
 def finalize(cobj):
     """
@@ -73,6 +81,7 @@ def finalize(cobj):
     """
     pass
 
+
 def set_gc(cobj, use):
     """
     set  GC to stop or not. test use only. don't use it.
@@ -82,6 +91,7 @@ def set_gc(cobj, use):
      """
     pass
 
+
 def gcollect(cobj):
     """
     run force garbage collect.
@@ -89,6 +99,7 @@ def gcollect(cobj):
     :param Object cobj: pointer of StorjTelehash instnace returned by init()
      """
     pass
+
 
 def get_my_location(cobj):
     """
@@ -101,4 +112,3 @@ def get_my_location(cobj):
      :return: location info.
      """
     pass
-
--- storj/messaging/storjtelehash/storjtelehash.py
+++ storj/messaging/storjtelehash/storjtelehash.py
@@ -34,8 +34,8 @@ import logging
 from storj.messaging.messaging import Messaging
 from storj.messaging.messaging import ChannelHandler
 from . import telehashbinder
+# import telehashbinder #for creating document
 
-#import telehashbinder #for creating document
 log_fmt = '%(filename)s:%(lineno)d %(funcName)s() %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
