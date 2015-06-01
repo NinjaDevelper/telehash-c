43294fc3c1dc7ba4ad4df1bc889e50b76820966a
utamaro <world.has.been.changed@gmail.com>
2015-06-01 20:36:34 +0900

    standarized for python, changed submodule to subtree

--- setup.py
+++ setup.py
@@ -35,8 +35,6 @@ from distutils.core import setup, Extension
 LONG_DESCRIPTION = open('README.md').read()
 VERSION = '1.0'
 
-os.environ['CC'] = 'g++'
-
 install_requirements = []
 
 test_requirements = [
@@ -46,14 +44,54 @@ test_requirements = [
     'coveralls'
 ]
 
+telehash_src = [
+    'telehash-c/src/ext/stream.c',
+    'telehash-c/src/ext/block.c',
+    'telehash-c/src/pipe.c',
+    'telehash-c/src/unix/util_sys.c',
+    'telehash-c/src/unix/util.c',
+    'telehash-c/src/net/serial.c',
+    'telehash-c/src/net/udp4.c',
+    'telehash-c/src/net/loopback.c',
+    'telehash-c/src/net/tcp4.c',
+    'telehash-c/src/lib/socketio.c',
+    'telehash-c/src/lib/tgc.c',
+    'telehash-c/src/lib/hashname.c',
+    'telehash-c/src/lib/base32.c',
+    'telehash-c/src/lib/js0n.c',
+    'telehash-c/src/lib/chacha.c',
+    'telehash-c/src/lib/base64.c',
+    'telehash-c/src/lib/xht.c',
+    'telehash-c/src/lib/murmur.c',
+    'telehash-c/src/lib/lob.c',
+    'telehash-c/src/link.c',
+    'telehash-c/src/mesh.c',
+    'telehash-c/src/e3x/channel.c',
+    'telehash-c/src/e3x/cs1a/aes-internal.c',
+    'telehash-c/src/e3x/cs1a/aes-internal-enc.c',
+    'telehash-c/src/e3x/cs1a/cs1a.c',
+    'telehash-c/src/e3x/cs1a/hmac.c',
+    'telehash-c/src/e3x/cs1a/sha256.c',
+    'telehash-c/src/e3x/cs1a/uECC.c',
+    'telehash-c/src/e3x/cs1a/aes.c',
+    'telehash-c/src/e3x/cs2a_disabled.c',
+    'telehash-c/src/e3x/exchange.c',
+    'telehash-c/src/e3x/cs3a_disabled.c',
+    'telehash-c/src/e3x/e3x.c',
+    'telehash-c/src/e3x/self.c',
+    'telehash-c/src/e3x/event.c',
+    'telehash-c/src/e3x/cipher.c',
+    'telehash-c/src/util/uri.c',
+    'telehash-c/src/util/util.c',
+    'telehash-c/src/util/chunks.c',
+]
 
 module = Extension('storj.messaging.storjtelehash.telehashbinder',
                    ['cxx/telehashbinder_python.cpp',
-                    'cxx/StorjTelehash.cpp'],
-                   libraries=['telehash'],
-                   include_dirs=['telehash-c/unix', 'telehash-c/include',
-                                 'cxx'],
-                   library_dirs=['telehash-c'],
+                    'cxx/StorjTelehash.cpp'] + telehash_src,
+                    extra_compile_args = ['-fpermissive'],
+                    include_dirs=['telehash-c/unix', 'telehash-c/include',
+                                 'telehash-c/lib', 'cxx'],
                    )
 
 
